2c716ffddd5c5e9ca96d274709fbb832d6768d91
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-11-12 23:44:44 +0000

    Edit base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,3 +2,18 @@
 """
 Test BaseModel
 """
+
+import unittest
+from models.base_model import BaseModel
+
+
+class test_BaseModel(Unittest.Unittest):
+    """
+    test ModelBase
+    """
+
+    def check_name(self):
+        """ check for name input """
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+
