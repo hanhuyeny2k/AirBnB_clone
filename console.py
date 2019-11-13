5480cad0406ed3fbccb2df7e3c50ee06612e8001
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-12 17:49:07 -0800

    Using re.match instead of re.fullmatch in update()

--- console.py
+++ console.py
@@ -161,14 +161,14 @@ class HBNBCommand(cmd.Cmd):
         """Parse <class>.<command>(<args>) syntax"""
         ident = r"[A-Za-z_][A-Za-z0-9_]*"
         regex = r"(" + ident + r")\.(" + ident + r")\((.*)\)"
-        match = re.fullmatch(regex, line.strip())
+        match = re.match(regex, line.strip())
         if match:
             cls, cmd, args = match.groups()
             if cmd == "update" and "," in args:
                 inst, args = [s.strip() for s in args.split(",", maxsplit=1)]
                 entry = r"[^ \t:][^:]*:\s*[^ \t,][^,]*"
                 regex = r"\{(\s*" + entry + r"(\s*,\s*" + entry + r")*)?\s*}"
-                if re.fullmatch(regex, args):
+                if re.match(regex, args):
                     args = args[1:-1].split(",")
                     args = [s.split(":", maxsplit=1) for s in args]
                     if all(len(ls) == 2 for ls in args):
