66683ba3b3edacffd0bc100917b6454180526d46
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-13 15:35:48 -0800

    Indented functions in test_BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,8 +14,10 @@ class test_BaseModel(Unittest.Unittest):
     """
     test ModelBase
     """
-    def setUp(self)
-    def teardown(self)
+    def setUp(self):
+        pass
+
+    def teardown(self):
         if path.exists(file.json):
             remove(file.json)
 
@@ -27,43 +29,43 @@ class test_BaseModel(Unittest.Unittest):
         self.assertIn('save', my_model)
         self.assertIn('to_dict', my_model)
 
-    def test_copy(self)
+    def test_copy(self):
         my_model = dir(BaseModel())
         self.assertIn('__init__', my_model)
         self.assertIn('__str__', my_model)
         self.assertIn('save', my_model)
         self.assertIn('to_dict', my_model)
 
-    def test_correctly_copy(self)
+    def test_correctly_copy(self):
         my_model = BaseModel()
         self.assertIsInstance(my_model.id, str)
         self.assertIsInstance(my_model.created_at, datetime.datetime)
         self.assertIsInstance(my_model.updated_at, datetime.datetime)
         self.assertIsInstance(my_model.__class__, type)
 
-    def test_for_str(self)
+    def test_for_str(self):
         my_model = BaseModel()
         string = '['+my_model.__class__.__name__+']' +' ('+my_model.id+')
                     '+str(my_model.__dict__)
         self.assertEqual(string, my_model.__str__())
 
-    def test_save
-    my_model = BaseModel()
-    my_storage = FileStorage()
-    update = my_model.__dict__['updated_at']
-    self.assertFalse(path.isfile('file.json'))
-    my_model.save()
-    self.assertTrue(path.isfile('file.json'))
-    self.assertNotEqual(my_model.__dict__['update_at'], update)
-    update = my_model.__dict__['updated_at']
-    my_storage.reload()
-    self.assertEqual(my_model.__dict__['update_at'], update)
+    def test_save(self):
+        my_model = BaseModel()
+        my_storage = FileStorage()
+        update = my_model.__dict__['updated_at']
+        self.assertFalse(path.isfile('file.json'))
+        my_model.save()
+        self.assertTrue(path.isfile('file.json'))
+        self.assertNotEqual(my_model.__dict__['update_at'], update)
+        update = my_model.__dict__['updated_at']
+        my_storage.reload()
+        self.assertEqual(my_model.__dict__['update_at'], update)
 
     def test_to_dict
-    my_model = BaseModel
-    self.assertEqual(my_model.to_dict()['__class__'],
-                     my_model.__class__.__name__)
-    self.assertEqual(my_model.to_dict()["updated_at"],
-                     my_model.updated_at.isoformat())
-    self.assertEqual(my_model.to_dict()["created_at"],
-                     my_model.created_at.isoformat())
+        my_model = BaseModel
+        self.assertEqual(my_model.to_dict()['__class__'],
+                         my_model.__class__.__name__)
+        self.assertEqual(my_model.to_dict()["updated_at"],
+                         my_model.updated_at.isoformat())
+        self.assertEqual(my_model.to_dict()["created_at"],
+                         my_model.created_at.isoformat())
